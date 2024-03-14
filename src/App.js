6b1706f851625635307245961e448266cc2ad200
Ryan khan <121855223+ryankhan0302@users.noreply.github.com>
2024-03-15 04:18:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=7b6c4078e69903d9695c65ee6eb54e04
+import React from 'react'
+import Temp from './temp'
+
+const App = () => {
+  return  <>
+      <Temp/>
+    </>
+
+}
+
+export default App
